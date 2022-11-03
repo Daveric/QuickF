4e705fe0c55caa83df3287a8f920f4edf7fb128b
brayanloayza <godsarmybl@gmail.com>
2022-11-03 01:30:14 -0500

    feat: initial commit

---
+++ api/src/main/java/com/creapption/quickf/QuickfApplication.java
@@ -0,0 +1,13 @@
+package com.creapption.quickf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/creapption/quickf/QuickfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creapption.quickf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
