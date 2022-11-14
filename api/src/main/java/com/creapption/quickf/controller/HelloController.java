f107a701175f9164ef2ac5555aa2c5929d309bc9
Erick Maldonado <e.maldonado@hundegger.com>
2022-11-14 23:33:11 +0100

    xades-bes for signing xml files implemented

--- api/src/main/java/com/creapption/quickf/controller/HelloController.java
+++ api/src/main/java/com/creapption/quickf/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.creapption.quickf.controller;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,26 +9,29 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import xades4j.algorithms.EnvelopedSignatureTransform;
-import xades4j.production.DataObjectReference;
-import xades4j.production.SignedDataObjects;
+import xades4j.production.BasicSignatureOptions;
+import xades4j.production.Enveloped;
 import xades4j.production.XadesBesSigningProfile;
 import xades4j.production.XadesSigner;
-import xades4j.properties.DataObjectDesc;
 import xades4j.providers.impl.FileSystemKeyStoreKeyingDataProvider;
 import xades4j.providers.impl.KeyStoreKeyingDataProvider;
 import xades4j.utils.DOMHelper;
 
+import java.io.OutputStream;
+import java.nio.file.Paths;
+import org.w3c.dom.Node;
+
 @RestController
 public class HelloController {
     private static final String password = "Loayzaq2022*";
+    private final static String path = String.format("%s%s", Paths.get("").toAbsolutePath().toString(), "\\api\\src\\main\\resources\\");
 
     /**
      * @return
@@ -44,35 +46,54 @@ public class HelloController {
     /**
      * @throws Exception
      */
-    public static void signBes() throws Exception{
+    public static void signBes() throws Exception{   
         Document doc = DocumentBuilderFactory
                 .newInstance()
                 .newDocumentBuilder()
-                .parse(new File("/Users/brayanloayza/Projects/quickf/api/src/main/resources/factura_without_signature.xml"));
+                .parse(new File(path+"factura_without_signature.xml"));
         Element elem = doc.getDocumentElement();
         DOMHelper.useIdAsXmlId(elem);
 
+        // Document doc = getDocument();
+        // Element elem = doc.getDocumentElement();
+        
         FileSystemKeyStoreKeyingDataProvider kp = FileSystemKeyStoreKeyingDataProvider
-        .builder("pkcs12", 
-        "/Users/brayanloayza/Projects/quickf/api/src/main/resources/LOAYZA_BRAYAN.p12",
-        KeyStoreKeyingDataProvider.SigningCertificateSelector.single())
+        .builder("pkcs12", path+"LOAYZA_BRAYAN.p12", KeyStoreKeyingDataProvider.SigningCertificateSelector.single())
         .storePassword(new DirectPasswordProvider(password))
         .entryPassword(new DirectPasswordProvider(password))
         .fullChain(true)
-        .provider(new BouncyCastleProvider())
         .build();
-
-        DataObjectDesc obj = new DataObjectReference("")
-                .withTransform(new EnvelopedSignatureTransform());
-        SignedDataObjects dataObjs = new SignedDataObjects().withSignedDataObject(obj);
-
-        XadesSigner signer = new XadesBesSigningProfile(kp).newSigner();
-        signer.sign(dataObjs, elem);
         
-        TransformerFactory tFactory = TransformerFactory.newInstance();
-        Transformer transformer = tFactory.newTransformer();
-        DOMSource source = new DOMSource(doc);        
-        StreamResult result = new StreamResult(new File("/Users/brayanloayza/Projects/quickf/api/src/main/resources/result.xml"));
-        transformer.transform(source, result);
+        XadesSigner signer = new XadesBesSigningProfile(kp)
+        .withBasicSignatureOptions(new BasicSignatureOptions().includePublicKey(true)).newSigner();
+        new Enveloped(signer).sign(elem);
+        
+        outputDocument(doc, "document.signed.bes.ec.xml");
+    }
+        
+    protected static void outputDocument(Document doc, String fileName) throws Exception
+    {
+        File outDir = ensureOutputDir();
+        FileOutputStream out = new FileOutputStream(new File(outDir, fileName));
+        try {
+            outputDOM(doc, out);
+        } finally {
+            out.close();
+        }
+    }
+    
+    protected static void outputDOM(Node dom, OutputStream os) throws Exception
+    {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        tf.newTransformer().transform(
+                new DOMSource(dom),
+                new StreamResult(os));
+    }
+    
+    private static File ensureOutputDir()
+    {
+        File dir = new File(path);
+        dir.mkdir();
+        return dir;
     }
 }
